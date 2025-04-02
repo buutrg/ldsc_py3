25ee043fae58c704d620aa5caff4f70231d00e7c
btruong <btruong@buutrgs-MacBook-Pro-1107.local>
2025-04-02 11:09:58 -0400

    Replace xrange with range for Python 3 compatibility

--- ldscore/irwls.py
+++ ldscore/irwls.py
@@ -109,10 +109,10 @@ class IRWLS(object):
                 'w has shape {S}. w must have shape ({N}, 1).'.format(S=w.shape, N=n))
 
         w = np.sqrt(w)
-        for i in xrange(2):  # update this later
+        for i in range(2):  # Changed xrange to range for Python 3
             new_w = np.sqrt(update_func(cls.wls(x, y, w)))
             if new_w.shape != w.shape:
-                print 'IRWLS update:', new_w.shape, w.shape
+                print('IRWLS update:', new_w.shape, w.shape)  # Changed to Python 3 print function
                 raise ValueError('New weights must have same shape.')
             else:
                 w = new_w
