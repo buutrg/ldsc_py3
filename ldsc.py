7272d9e7787fb674d95225b1d466ef692b66b417
btruong <btruong@buutrgs-MacBook-Pro-1107.local>
2025-04-02 11:14:58 -0400

    Update remaining print statements and comments for Python 3 compatibility

--- ldsc.py
+++ ldsc.py
@@ -328,13 +328,13 @@ def ldscore(args, log):
         M_5_50 = [np.sum(geno_array.maf > 0.05)]
 
     # print .M
-    fout_M = open(args.out + '.'+ file_suffix +'.M','wb')
-    print >>fout_M, '\t'.join(map(str,M))
+    fout_M = open(args.out + '.'+ file_suffix +'.M','w')
+    print('\t'.join(map(str,M)), file=fout_M)
     fout_M.close()
 
     # print .M_5_50
-    fout_M_5_50 = open(args.out + '.'+ file_suffix +'.M_5_50','wb')
-    print >>fout_M_5_50, '\t'.join(map(str,M_5_50))
+    fout_M_5_50 = open(args.out + '.'+ file_suffix +'.M_5_50','w')
+    print('\t'.join(map(str,M_5_50)), file=fout_M_5_50)
     fout_M_5_50.close()
 
     # print annot matrix
@@ -615,9 +615,9 @@ if __name__ == '__main__':
 
             # bad flags
         else:
-            print header
-            print 'Error: no analysis selected.'
-            print 'ldsc.py -h describes options.'
+            print(header)
+            print('Error: no analysis selected.')
+            print('ldsc.py -h describes options.')
     except Exception:
         ex_type, ex, tb = sys.exc_info()
         log.log( traceback.format_exc(ex) )
